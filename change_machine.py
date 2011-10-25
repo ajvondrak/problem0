688750a88bdca2dcbaf9298b2c7633d24a25f774
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:41:25 -0700

    save a few bytes by being destructive

--- change_machine.py
+++ change_machine.py
@@ -1,8 +1,8 @@
 import sys
 for i in map(int,sys.stdin.readlines()):
  print i,"cents:"
- if (i/25):print i/25,"x 25 cents"
- if (i%25/10):print i%25/10,"x 10 cents"
- if (i%25%10/5):print i%25%10/5,"x 5 cents"
- if (i%25%10%5):print i%25%10%5,"x 1 cents"
+ if i/25:print i/25,"x 25 cents";i%=25
+ if i/10:print i/10,"x 10 cents";i%=10
+ if i/5:print i/5,"x 5 cents";i%=5
+ if i:print i,"x 1 cents"
  print
