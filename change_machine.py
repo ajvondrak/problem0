ff4f5b600c07820150594c8bf1a6582fec40dfae
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:25:38 -0700

    merge loop and function

--- change_machine.py
+++ change_machine.py
@@ -5,12 +5,10 @@ def deduct(cents, denom):
     if n > 0: print "%d x %d cents" % (n, denom)
     return cents
 
-def change_machine(cents):
+for cents in map(int, sys.stdin.readlines()):
     print "%d cents:" % cents
     cents = deduct(cents, 25)
     cents = deduct(cents, 10)
     cents = deduct(cents, 5)
     deduct(cents, 1)
     print
-
-for line in sys.stdin.readlines(): change_machine(int(line))
