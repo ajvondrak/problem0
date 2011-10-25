674aa351cdfb33b35a25213bd676427d3d0cbfd8
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:20:22 -0700

    factor out common logic

--- change_machine.py
+++ change_machine.py
@@ -1,32 +1,19 @@
 import sys
 
-def print_denom(n, denom):
-    if n > 0: print "%d x %d cents" % (n, denom)
+def deduct(cents, denom):
+    n = 0
+    while cents >= denom:
+        cents -= denom
+        n += 1
+    if n > 0:
+        print "%d x %d cents" % (n, denom)
+    return cents
 
 def change_machine(cents):
-    quarters, dimes, nickels, pennies = 0, 0, 0, 0
-
-    while cents >= 25:
-        cents -= 25
-        quarters += 1
-    print_denom(quarters, 25)
-
-    while cents >= 10:
-        cents -= 10
-        dimes += 1
-    print_denom(dimes, 10)
-
-    while cents >= 5:
-        cents -= 5
-        nickels += 1
-    print_denom(nickels, 5)
-
-    while cents >= 1:
-        cents -= 1
-        pennies += 1
-    print_denom(pennies, 1)
-
-for line in sys.stdin.readlines():
-    cents = int(line)
     print "%d cents:" % cents
-    print 
+    cents = deduct(cents, 25)
+    cents = deduct(cents, 10)
+    cents = deduct(cents, 5)
+    deduct(cents, 1)
+
+for line in sys.stdin.readlines(): change_machine(int(line))
