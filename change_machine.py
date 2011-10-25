58ba8176db7f60f0c419fe8c1f9693ec21df67f5
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 14:29:14 -0700

    save 1 byte using a positive index in the slice instead of a negative; total = 188 bytes

--- change_machine.py
+++ change_machine.py
@@ -1,6 +1,6 @@
 import sys
 for i in map(int,sys.stdin):
- x="x %s cents";print i,x[-5:]+":"
+ x="x %s cents";print i,x[5:]+":"
  if i/25:print i/25,x%25;i%=25
  if i/10:print i/10,x%10;i%=10
  if i/5:print i/5,x%5;i%=5
