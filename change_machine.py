2683b5729a42251fcfd8965279f5205b002f3440
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:36:29 -0700

    golf syntax

--- change_machine.py
+++ change_machine.py
@@ -1,9 +1,8 @@
 import sys
-
-for cents in map(int, sys.stdin.readlines()):
-    print cents, "cents:"
-    if (cents/25): print cents/25, "x 25 cents"
-    if (cents%25/10): print cents%25/10, "x 10 cents"
-    if (cents%25%10/5): print cents%25%10/5, "x 5 cents"
-    if (cents%25%10%5): print cents%25%10%5, "x 1 cents"
+for i in map(int,sys.stdin.readlines()):
+    print i,"cents:"
+    if (i/25):print i/25,"x 25 cents"
+    if (i%25/10):print i%25/10,"x 10 cents"
+    if (i%25%10/5):print i%25%10/5,"x 5 cents"
+    if (i%25%10%5):print i%25%10%5,"x 1 cents"
     print
