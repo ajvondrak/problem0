d1f6d4bfe8070966485053cd89847dafb1ea9c40
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 14:30:55 -0700

    save 1 byte not being destructive for the pennies mod; total = 187 bytes

--- change_machine.py
+++ change_machine.py
@@ -3,6 +3,6 @@ for i in map(int,sys.stdin):
  x="x %s cents";print i,x[5:]+":"
  if i/25:print i/25,x%25;i%=25
  if i/10:print i/10,x%10;i%=10
- if i/5:print i/5,x%5;i%=5
- if i:print i,x%1
+ if i/5:print i/5,x%5
+ if i%5:print i%5,x%1
  print
