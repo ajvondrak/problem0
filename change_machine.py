b6ac752de8e559870734df1f8503124e7ed592db
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:53:07 -0700

    apparently readlines() is the implicit iterator for sys.stdin

--- change_machine.py
+++ change_machine.py
@@ -1,5 +1,5 @@
 import sys
-for i in map(int,sys.stdin.readlines()):
+for i in map(int,sys.stdin):
  c="cents";x="x";print i,c+":"
  if i/25:print i/25,x,25,c;i%=25
  if i/10:print i/10,x,10,c;i%=10
