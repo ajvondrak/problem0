ee6c723d0f50a32ef013ba5236df50ef9af12413
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 13:38:25 -0700

    save a few bytes using 1 space for indentation instead of 4

--- change_machine.py
+++ change_machine.py
@@ -1,8 +1,8 @@
 import sys
 for i in map(int,sys.stdin.readlines()):
-    print i,"cents:"
-    if (i/25):print i/25,"x 25 cents"
-    if (i%25/10):print i%25/10,"x 10 cents"
-    if (i%25%10/5):print i%25%10/5,"x 5 cents"
-    if (i%25%10%5):print i%25%10%5,"x 1 cents"
-    print
+ print i,"cents:"
+ if (i/25):print i/25,"x 25 cents"
+ if (i%25/10):print i%25/10,"x 10 cents"
+ if (i%25%10/5):print i%25%10/5,"x 5 cents"
+ if (i%25%10%5):print i%25%10%5,"x 1 cents"
+ print
