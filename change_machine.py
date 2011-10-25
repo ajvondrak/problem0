f3aaca9b2b3242663956e6c9e46c0c1a444bd1ff
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-25 14:18:24 -0700

    save 2 bytes using a format string

--- change_machine.py
+++ change_machine.py
@@ -1,8 +1,8 @@
 import sys
 for i in map(int,sys.stdin):
- c="cents";x="x";print i,c+":"
- if i/25:print i/25,x,25,c;i%=25
- if i/10:print i/10,x,10,c;i%=10
- if i/5:print i/5,x,5,c;i%=5
- if i:print i,x,1,c
+ c="cents";x="x %s "+c;print i,c+":"
+ if i/25:print i/25,x%25;i%=25
+ if i/10:print i/10,x%10;i%=10
+ if i/5:print i/5,x%5;i%=5
+ if i:print i,x%1
  print
