6d8d993f619d1416709c842bae94a3a115146f7b
Alex Vondrak <ajvondrak@csupomona.edu>
2011-10-28 16:31:50 -0700

    remove some useless semicolons/parens and use $/ instead of "\n"; total = 101 bytes

--- change_machine.pl
+++ change_machine.pl
@@ -1,1 +1,1 @@
-map{$i=$_+0;print"$i cents:\n";map{print"$q x $_ cents\n"if$q=int($i/$_);$i%=$_;}(25,10,5,1);print"\n";}<>
+map{$i=$_+0;print"$i cents:\n";map{print"$q x $_ cents\n"if$q=int$i/$_;$i%=$_}(25,10,5,1);print$/}<>
