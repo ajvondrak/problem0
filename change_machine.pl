98c7d4be216e11108b1ff8b59962eb365870c950
Alex Vondrak <alex@alex-pc>
2011-10-29 06:09:50 -0700

    save 2 more bytes in the perl version by removing some parens; total = 97 bytes

--- change_machine.pl
+++ change_machine.pl
@@ -1,1 +1,1 @@
-map{print$i=$_+0," cents:\n";map{print"$q x $_ cents\n"if$q=int$i/$_;$i%=$_}(25,10,5,1);print$/}<>
+map{print$i=$_+0," cents:\n";map{print"$q x $_ cents\n"if$q=int$i/$_;$i%=$_}25,10,5,1;print$/}<>
